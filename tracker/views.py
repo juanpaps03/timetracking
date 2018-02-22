a7bf1c6637767418c9aa48f13bf412b0736c7ca5
Mathias <mathias.lanean@hotmail.com>
2018-02-22 00:42:07 -0300

    WIP: serialize data to send to the server

--- tracker/views.py
+++ tracker/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.views import View
 
 from sabyltimetracker.users.models import User
+from tracker.models import Task, Building
 from sabyltimetracker.users.users_permissions_decorator import user_permissions
 
 
@@ -19,5 +20,16 @@ class Dashboard(View):
 @method_decorator([user_permissions([User.OVERSEER])], name='dispatch')
 class LogHours(View):
     def get(self, request, username):
-        # <view logic>
-        return render(request, 'tracker/log_hours.html')
+        tasks = []
+        workers = []
+        user = request.user
+
+        # Select the last building related to the overseer then obtain its tasks and workers
+        building = Building.objects.filter(overseer=user).last()
+        if building:
+            tasks = building.tasks.all()
+            workers = building.workers.all()
+
+        context = {'tasks': tasks, 'workers': workers}
+
+        return render(request, 'tracker/log_hours.html', context)
