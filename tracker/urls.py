ef7af7b0c3ceef307624a56b0c0918ec6df921ab
Mathias Lantean <mathias.lanean@hotmail.com>
2018-02-23 07:48:56 -0300

    WIP: add hours to each user

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from . import views
+from . import views, views_api
 
 urlpatterns = [
     url(regex=r'^$', view=views.Dashboard.as_view(), name='dashboard'),
     url(regex=r'^loghours/$', view=views.LogHours.as_view(), name='log_hours'),
+    url(regex=r'^api/add_hours$', view=views_api.LogsHoursCrate.as_view(), name='api_add_hours'),
 ]
--- tracker/views_api.py
+++ tracker/views_api.py
@@ -1,7 +1,25 @@
+import datetime
+
 from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework.parsers import JSONParser
+
+from django.http import JsonResponse
+
+from tracker.models import Building, Workday
 
 
-class LogsHoursCreate(generics.CreateAPIView):
-    pass
+class LogsHoursCrate(APIView):
+
+    def post(self, request, username):
+        user = request.user
+        building = Building.objects.filter(overseer=user).last()
+        date = datetime.date.today()
+        try:
+            work_day = Workday.objects.get(building=building, date=date, finished=False)
+        except Workday.DoesNotExist:
+            return JsonResponse({'error': 'There was a problem obtaining work day. Maybe it was finished.'})
+
+        return JsonResponse({'test': 'test'})
+
 
