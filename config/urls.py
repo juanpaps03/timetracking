579b5dfa749d9cc35e51808b9452dd4e63756c6e
juanpablo <juanpaps03@gmail.com>
2018-03-21 17:06:01 -0300

    Quick change to redirect users

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,11 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
+from sabyltimetracker.users.views import UserRedirectView
+
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    #url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', UserRedirectView.as_view(), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
