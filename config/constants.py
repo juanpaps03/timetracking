a9e57f8a003cbd53da6aa361f073edacb59e547d
gonzalo.montes <gonzalomdeoca@gmail.com>
2021-02-11 20:44:16 -0300

    Se agrega tarea LLS a tareas que no suman.

--- config/constants.py
+++ config/constants.py
@@ -116,13 +116,13 @@ DYNAMIC_CONSTANT_FIELDSETS = OrderedDict([
 ])
 
 
-DIAS_DE_HORAS_EXTRA = ["07/05/2021", "01/05/2020", "02/05/2020", "03/05/2020"]
+DIAS_DE_HORAS_EXTRA = ["01/05/2020", "02/05/2020", "03/05/2020"]
 
 
-TAREAS_QUE_NO_SUMAN = ["AS", "DS", "E", "F", "FA", "LD", "LE", "LHD", "LL", "LM", "LP", "P", "PG", "SA", "FOCAP", "FC", "T", "LA", "S", "XAS", "XDS", "XE", "XF", "XFA", "XLD", "XLE", "XLHD", "XLM", "XLP", "XP", "XPG", "XSA", "XFOCAP", "XFC", "XT", "XLA", "XS"];
+TAREAS_QUE_NO_SUMAN = ["AS", "DS", "E", "F", "FA", "LD", "LE", "LHD", "LL", "LM", "LP", "P", "PG", "SA", "FOCAP", "FC", "T", "LA", "S", "XAS", "XDS", "XE", "XF", "XFA", "XLD", "XLE", "XLHD", "XLM", "XLP", "XP", "XPG", "XSA", "XFOCAP", "XFC", "XT", "XLA", "XS", "LLS"];
 
 
-TAREAS_VARIOS_TRABAJADORES = ["XP", "XPG", "XAS", "XCH", "LL"];
+TAREAS_VARIOS_TRABAJADORES = ["XP", "XPG", "XAS", "XCH", "LL", "LLS"];
 
 
 TAREAS_ESPECIALES_TODO_EL_DIA = ["XDS", "XE", "XF", "XFA", "XLD", "XLE", "XLM", "XLP", "XPG", "XFC", "XT", "XLA", "XS"];
